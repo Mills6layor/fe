cb71f3f6d02422dc575ef9d02da8c10183266085
Mills6layor <160451944+Mills6layor@users.noreply.github.com>
2024-02-20 14:23:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-free flask import Flask,request,render_template
+from flask import Flask,request,render_template
 
 app=Flask(_name_)
 
-@app,route("/",methods=["GET,"POST"])
+@app,route("/",methods=["GET","POST"])
 def index();
  return(render_template("index,html"))
+
+@app.route("/main",methods=["GET","POST"])
+ def main（）：
+name=request,form,get("name")
+return(rander_templates("main,html",r=name))
 
 if_name_=="_main_";
  app.run()
